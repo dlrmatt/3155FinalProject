fb65051dc5e4127a645e90da0f4fac9a8683ffb2
matthewdelarosa <mdelaro2@charlotte.edu>
2026-04-15 11:19:29 -0400

    fix: db changes
    Changed code to connect properly with new db.

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, menu_items, resources
+from . import orders, order_details, menu_items, resources, customers, payments, promotions, reviews
 
 from ..dependencies.database import engine
 
@@ -6,6 +6,9 @@ from ..dependencies.database import engine
 def index():
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
+    menu_items.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    customers.Base.metadata.create_all(engine)
+    payments.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
